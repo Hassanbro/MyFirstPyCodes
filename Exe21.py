6bcde2ea0f18e206650da9c810853c9566e925b8
Hassan <hassanbro@gmail.com>
2020-10-17 19:42:40 +0000

    Initial commit

---
+++ Exe21.py
@@ -0,0 +1,22 @@
+b = 20
+a = 10
+
+# == THIS FOR COMPARISION AND ALWAYS HAVE A BOOLEAN OUTPUT
+
+print(b == a)
+
+#<
+
+print(b < a)
+
+#>
+
+print(b > a)
+
+#<=
+
+print(b <= a)
+
+#>=
+
+print(b >= a)
---
+++ main.py
@@ -0,0 +1,14 @@
+b = 20
+a = 10
+
+# == THIS FOR COMPARISION AND ALWAYS HAVE A BOOLEAN OUTPUT
+
+print(b == a)
+
+#<
+
+print(b < a)
+
+#>
+
+print(b > a)
